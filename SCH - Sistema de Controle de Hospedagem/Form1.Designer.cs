86d80891b704ee1fc6bfce5bda2906f29a495d8f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2023-02-20 19:35:21 -0300

    Criação: validação de campus e grid de dados para o usuário.

--- SCH - Sistema de Controle de Hospedagem/Form1.Designer.cs
+++ SCH - Sistema de Controle de Hospedagem/Form1.Designer.cs
@@ -31,10 +31,10 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
         {
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtEndereco = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblTelCel = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.mskTxtCPF = new System.Windows.Forms.MaskedTextBox();
             this.mskTxtTelCel = new System.Windows.Forms.MaskedTextBox();
@@ -42,14 +42,22 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNome
             // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.Window;
             this.txtNome.Enabled = false;
+            this.txtNome.ForeColor = System.Drawing.SystemColors.WindowText;
             this.txtNome.Location = new System.Drawing.Point(74, 57);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(240, 20);
+            this.txtNome.Size = new System.Drawing.Size(323, 20);
             this.txtNome.TabIndex = 0;
             // 
             // txtEndereco
@@ -57,50 +65,55 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             this.txtEndereco.Enabled = false;
             this.txtEndereco.Location = new System.Drawing.Point(74, 83);
             this.txtEndereco.Name = "txtEndereco";
-            this.txtEndereco.Size = new System.Drawing.Size(240, 20);
+            this.txtEndereco.Size = new System.Drawing.Size(323, 20);
             this.txtEndereco.TabIndex = 1;
             // 
-            // label1
+            // lblNome
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nome:";
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(23, 62);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(44, 15);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
             // 
-            // label2
+            // lblEndereco
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Endereço:";
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndereco.Location = new System.Drawing.Point(5, 88);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(63, 15);
+            this.lblEndereco.TabIndex = 0;
+            this.lblEndereco.Text = "Endereço:";
             // 
-            // label3
+            // lblCPF
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 116);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "CPF:";
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(31, 114);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(33, 15);
+            this.lblCPF.TabIndex = 0;
+            this.lblCPF.Text = "CPF:";
             // 
-            // label4
+            // lblTelCel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 142);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Tel. Cel.:";
+            this.lblTelCel.AutoSize = true;
+            this.lblTelCel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelCel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTelCel.Location = new System.Drawing.Point(12, 140);
+            this.lblTelCel.Name = "lblTelCel";
+            this.lblTelCel.Size = new System.Drawing.Size(54, 15);
+            this.lblTelCel.TabIndex = 0;
+            this.lblTelCel.Text = "Tel. Cel.:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(124, 9);
+            this.label5.Location = new System.Drawing.Point(239, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(372, 31);
             this.label5.TabIndex = 6;
@@ -110,25 +123,27 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             // 
             this.mskTxtCPF.Enabled = false;
             this.mskTxtCPF.Location = new System.Drawing.Point(74, 109);
+            this.mskTxtCPF.Mask = "000.000.000-00";
             this.mskTxtCPF.Name = "mskTxtCPF";
-            this.mskTxtCPF.Size = new System.Drawing.Size(154, 20);
-            this.mskTxtCPF.TabIndex = 7;
+            this.mskTxtCPF.Size = new System.Drawing.Size(82, 20);
+            this.mskTxtCPF.TabIndex = 2;
             // 
             // mskTxtTelCel
             // 
             this.mskTxtTelCel.Enabled = false;
             this.mskTxtTelCel.Location = new System.Drawing.Point(74, 135);
+            this.mskTxtTelCel.Mask = "(00) 00000-0000";
             this.mskTxtTelCel.Name = "mskTxtTelCel";
-            this.mskTxtTelCel.Size = new System.Drawing.Size(154, 20);
-            this.mskTxtTelCel.TabIndex = 8;
+            this.mskTxtTelCel.Size = new System.Drawing.Size(85, 20);
+            this.mskTxtTelCel.TabIndex = 4;
             // 
             // btnNovoCadastro
             // 
             this.btnNovoCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNovoCadastro.Location = new System.Drawing.Point(130, 237);
+            this.btnNovoCadastro.Location = new System.Drawing.Point(23, 185);
             this.btnNovoCadastro.Name = "btnNovoCadastro";
             this.btnNovoCadastro.Size = new System.Drawing.Size(109, 33);
-            this.btnNovoCadastro.TabIndex = 9;
+            this.btnNovoCadastro.TabIndex = 1;
             this.btnNovoCadastro.Text = "Novo Cadastro";
             this.btnNovoCadastro.UseVisualStyleBackColor = true;
             this.btnNovoCadastro.Click += new System.EventHandler(this.btnNovoCadastro_Click);
@@ -137,10 +152,10 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             // 
             this.btnSalvar.Enabled = false;
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalvar.Location = new System.Drawing.Point(245, 237);
+            this.btnSalvar.Location = new System.Drawing.Point(138, 185);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(55, 33);
-            this.btnSalvar.TabIndex = 10;
+            this.btnSalvar.TabIndex = 5;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
@@ -149,10 +164,10 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             // 
             this.btnCancelar.Enabled = false;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(306, 237);
+            this.btnCancelar.Location = new System.Drawing.Point(324, 185);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(73, 33);
-            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.TabIndex = 7;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
@@ -161,19 +176,68 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             // 
             this.btnExcluir.Enabled = false;
             this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExcluir.Location = new System.Drawing.Point(385, 237);
+            this.btnExcluir.Location = new System.Drawing.Point(259, 185);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(59, 33);
-            this.btnExcluir.TabIndex = 12;
+            this.btnExcluir.TabIndex = 6;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
             this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(199, 185);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(54, 33);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(516, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Buscar:";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.BackColor = System.Drawing.SystemColors.Window;
+            this.txtBuscar.Enabled = false;
+            this.txtBuscar.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtBuscar.Location = new System.Drawing.Point(565, 64);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(323, 20);
+            this.txtBuscar.TabIndex = 10;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(15, 238);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.Size = new System.Drawing.Size(537, 140);
+            this.dataGrid.TabIndex = 11;
+            this.dataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 299);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnSalvar);
@@ -181,15 +245,16 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             this.Controls.Add(this.mskTxtTelCel);
             this.Controls.Add(this.mskTxtCPF);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTelCel);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNome);
             this.Controls.Add(this.txtEndereco);
             this.Controls.Add(this.txtNome);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,10 +264,10 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
 
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtEndereco;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblTelCel;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.MaskedTextBox mskTxtCPF;
         private System.Windows.Forms.MaskedTextBox mskTxtTelCel;
@@ -210,6 +275,11 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- SCH - Sistema de Controle de Hospedagem/Form1.cs
+++ SCH - Sistema de Controle de Hospedagem/Form1.cs
@@ -24,17 +24,41 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            ListagemGridDB();
         }
 
         private void btnNovoCadastro_Click(object sender, EventArgs e)
         {
-            Des_Habiliatar(true);
+            Des_Habiliatar(0, true);
             txtNome.Focus();
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            if(txtNome.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Preencha o campo nome!");
+                txtNome.Text = "";
+                txtNome.Focus();
+                return;
+            } 
+            else if (!ValidaCPF(mskTxtCPF.Text))
+            {
+                MessageBox.Show("Esse CPF é invalido!");
+                mskTxtCPF.Text = "";
+                mskTxtCPF.Focus();
+                return;
+            }
+            else if( mskTxtTelCel.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Preencha o campo Telefone!");
+                mskTxtTelCel.Text = "";
+                mskTxtTelCel.Focus();
+                return;
+            }
+
+
+            Des_Habiliatar(1, false);
             conexao.AbrirConcexao(); // Abre a conexão com o banco de dados
 
             // Define a query SQL para inserir um novo cliente
@@ -46,44 +70,224 @@ namespace SCH___Sistema_de_Controle_de_Hospedagem
             // Define os parâmetros da query SQL, passando os valores dos campos de cadastro
             cmd.Parameters.AddWithValue("@nome", txtNome.Text);
             cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
-            cmd.Parameters.AddWithValue("@cpf", mskTxtCPF.Text);
+            cmd.Parameters.AddWithValue("@cpf", mskTxtCPF.Text.Replace(",", "."));
             cmd.Parameters.AddWithValue("@telefone", mskTxtTelCel.Text);
 
             cmd.ExecuteNonQuery(); // Executa a query SQL
             conexao.FecharConexao(); // Fecha a conexão com o banco de dados
 
-            Des_Habiliatar(false);
+            Des_Habiliatar(0, false);
+            ListagemGridDB();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-            Des_Habiliatar(false);
+            Des_Habiliatar(0, false);
+            btnExcluir.Enabled = false;
+            btnEditar.Enabled = false;
         }
 
         private void btnExcluir_Click(object sender, EventArgs e)
         {
-            Des_Habiliatar(false);
+            Des_Habiliatar(0, false);
+            btnExcluir.Enabled = false;
+            btnEditar.Enabled = false;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Preencha o campo nome!");
+                txtNome.Text = "";
+                txtNome.Focus();
+                return;
+            }
+            else if (!ValidaCPF(mskTxtCPF.Text))
+            {
+                MessageBox.Show("Esse CPF é invalido!");
+                mskTxtCPF.Text = "";
+                mskTxtCPF.Focus();
+                return;
+            }
+            else if (mskTxtTelCel.Text.ToString().Trim() == "")
+            {
+                MessageBox.Show("Preencha o campo Telefone!");
+                mskTxtTelCel.Text = "";
+                mskTxtTelCel.Focus();
+                return;
+            }
+
+
+            Des_Habiliatar(1, false);
+
+            conexao.AbrirConcexao(); // Abre a conexão com o banco de dados
+
+            // Define a query SQL para atualizar o cadastro de um cliente
+            sql = "UPDATE cliente SET nome=@nome, endereco=@endereco, cpf=@cpf, telefone=@telefone";
+
+            // Instancia um novo objeto MySqlCommand, passando a query SQL e a conexão com o banco de dados como parâmetros
+            cmd = new MySqlCommand(sql, conexao.conex);
+
+            // Define os parâmetros da query SQL, passando os valores dos campos de cadastro
+            cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+            cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+            cmd.Parameters.AddWithValue("@cpf", mskTxtCPF.Text.Replace(",", "."));
+            cmd.Parameters.AddWithValue("@telefone", mskTxtTelCel.Text);
+
+            cmd.ExecuteNonQuery(); // Executa a query SQL
+            conexao.FecharConexao(); // Fecha a conexão com o banco de dados
+
+            Des_Habiliatar(0, false);
+            ListagemGridDB();
+        }
+
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Des_Habiliatar(2, true);
+            txtNome.Text = dataGrid.CurrentRow.Cells[1].Value.ToString();
+            txtEndereco.Text = dataGrid.CurrentRow.Cells[2].Value.ToString();
+            mskTxtCPF.Text = dataGrid.CurrentRow.Cells[3].Value.ToString().Replace(".",",");
+            mskTxtTelCel.Text = dataGrid.CurrentRow.Cells[4].Value.ToString();
         }
 
         // Método que habilita ou desabilita a edição dos campos de cadastro
-        private void Des_Habiliatar(bool dh = false)
+        private void Des_Habiliatar(int txt = 0, bool dh = false)
         {
-            // Método que habilita ou desabilita a edição dos campos de cadastro
-            txtNome.Text = "";
-            txtEndereco.Text = "";
-            mskTxtCPF.Text = "";
-            mskTxtTelCel.Text = "";
+            if (txt == 0)
+            {
+                // Limpa os campos de cadastro
+                txtNome.Text = "";
+                txtEndereco.Text = "";
+                mskTxtCPF.Text = "";
+                mskTxtTelCel.Text = "";
 
-            // Define a propriedade Enabled de cada controle de acordo com o parâmetro do método
-            txtNome.Enabled = dh;
-            txtNome.Enabled = dh;
-            txtEndereco.Enabled = dh;
-            mskTxtCPF.Enabled = dh;
-            mskTxtTelCel.Enabled = dh;
-            btnNovoCadastro.Enabled = !dh;
-            btnSalvar.Enabled = dh;
-            btnExcluir.Enabled = dh;
-            btnCancelar.Enabled = dh;
+                // Define a propriedade Enabled de cada controle de acordo com o parâmetro do método
+                txtNome.Enabled = dh;
+                txtNome.Enabled = dh;
+                txtEndereco.Enabled = dh;
+                mskTxtCPF.Enabled = dh;
+                mskTxtTelCel.Enabled = dh;
+                btnNovoCadastro.Enabled = !dh;
+                btnSalvar.Enabled = dh;
+                btnExcluir.Enabled = !dh;
+                btnCancelar.Enabled = dh;
+                btnEditar.Enabled = !dh;
+
+            }
+            else if (txt == 1)
+            {
+                txtNome.Enabled = dh;
+                txtNome.Enabled = dh;
+                txtEndereco.Enabled = dh;
+                mskTxtCPF.Enabled = dh;
+                mskTxtTelCel.Enabled = dh;
+                btnNovoCadastro.Enabled = dh;
+                btnSalvar.Enabled = dh;
+                btnExcluir.Enabled = dh;
+                btnCancelar.Enabled = dh;
+                btnEditar.Enabled = dh;
+            }
+            else if(txt == 2)
+            {
+                txtNome.Enabled = dh;
+                txtNome.Enabled = dh;
+                txtEndereco.Enabled = dh;
+                mskTxtCPF.Enabled = dh;
+                mskTxtTelCel.Enabled = dh;
+                btnNovoCadastro.Enabled = !dh;
+                btnSalvar.Enabled = !dh;
+                btnExcluir.Enabled = dh;
+                btnCancelar.Enabled = dh;
+                btnEditar.Enabled = dh;
+            }
+        }
+
+        // O método ValidaCPF retorna True para um CPF válido e False para um CPF inválido
+        // Código da DevMedia: http://www.devmedia.com.br/articles/viewcomp_forprint.asp?comp=3950
+        public static bool ValidaCPF(string vrCPF)
+
+        {
+
+            string valor = vrCPF.Replace(",", "");
+            valor = valor.Replace("-", "");
+
+            if (valor.Length != 11)
+                return false;
+
+            bool igual = true;
+
+            for (int i = 1; i < 11 && igual; i++)
+                if (valor[i] != valor[0])
+                    igual = false;
+
+            if (igual || valor == "12345678909")
+                return false;
+
+            int[] numeros = new int[11];
+
+            for (int i = 0; i < 11; i++)
+                numeros[i] = int.Parse(valor[i].ToString());
+
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += (10 - i) * numeros[i];
+
+            int resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[9] != 0)
+                    return false;
+            }
+
+            else if (numeros[9] != 11 - resultado)
+                return false;
+
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += (11 - i) * numeros[i];
+
+            resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[10] != 0)
+                    return false;
+            }
+            else if (numeros[10] != 11 - resultado)
+                return false;
+
+            return true;
+
+        }
+
+        // Função para formatar as colunas do grid
+        private void FormatacaoGrid()
+        {
+            // Define os títulos das colunas do grid
+            dataGrid.Columns[0].HeaderText = "Código";
+            dataGrid.Columns[1].HeaderText = "Nome";
+            dataGrid.Columns[2].HeaderText = "Endereço";
+            dataGrid.Columns[3].HeaderText = "CPF";
+            dataGrid.Columns[4].HeaderText = "Telefone";
+        }
+
+        // Função para carregar os dados do banco de dados no grid
+        private void ListagemGridDB()
+        {
+            conexao.AbrirConcexao(); 
+            sql = "SELECT * FROM cliente ORDER BY NOME ASC"; // Define a consulta SQL para selecionar todos os registros da tabela cliente ordenados pelo nome
+            cmd = new MySqlCommand(sql, conexao.conex); 
+            MySqlDataAdapter da = new MySqlDataAdapter(); 
+            da.SelectCommand = cmd;  // Atribui o objeto do tipo MySqlCommand ao objeto do tipo MySqlDataAdapter
+            DataTable dt = new DataTable(); 
+            da.Fill(dt); // Preenche o objeto do tipo DataTable com os dados do banco de dados usando o objeto do tipo MySqlDataAdapter
+            dataGrid.DataSource = dt; // Atribui o objeto do tipo DataTable como fonte de dados do grid
+            conexao.FecharConexao(); 
+            FormatacaoGrid(); // Formata as colunas do grid
         }
 
     }
